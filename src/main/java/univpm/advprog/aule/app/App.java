a20c41d401496720c72c3aee6bb7167b4cea2279
mirkosimoni <mirko-simoni@live.it>
2019-12-16 16:01:03 +0100

    commiy

--- src/main/java/univpm/advprog/aule/app/App.java
+++ src/main/java/univpm/advprog/aule/app/App.java
@@ -1,13 +1,15 @@
 package univpm.advprog.aule.app;
 
 /**
+ * ciao
  * Hello world!
- *  
+ *  Fabio_prova
  */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
 }
