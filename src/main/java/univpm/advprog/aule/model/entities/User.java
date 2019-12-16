fa6f21c834e5800babba97f3f345550632fa21d5
albertopierini <50133437+albertopierini@users.noreply.github.com>
2019-12-16 16:07:55 +0100

    add Role

--- src/main/java/univpm/advprog/aule/model/entities/User.java
+++ src/main/java/univpm/advprog/aule/model/entities/User.java
@@ -1,5 +1,17 @@
 package univpm.advprog.aule.model.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 public class User {
 
+	@Id
+	  @Column(name = "USERNAME")
+	  private String username;
+
+	  @Column(name = "PASSWORD", nullable = false)
+	  private String password;
+
+	  @Column(name = "ENABLED", nullable = false)
+	  private boolean enabled;
 }
