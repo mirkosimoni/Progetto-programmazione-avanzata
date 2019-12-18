21ad5c4ef34ad2431ce37948dea39585f43da185
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2019-12-18 14:27:32 +0100

    add configuration to WebSecurityConfig

--- src/main/java/univpm/advprog/aule/security/WebSecurityConfig.java
+++ src/main/java/univpm/advprog/aule/security/WebSecurityConfig.java
@@ -1,6 +1,13 @@
 package univpm.advprog.aule.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 //Classe in cui andrnno impostati i permessi dei vari utenti
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
 }
