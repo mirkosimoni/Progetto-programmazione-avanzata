e161ce135e923cc8e4b43ae68bf34d2ea4e22c60
albertopierini <50133437+albertopierini@users.noreply.github.com>
2019-12-18 13:48:56 +0100

    Completamento Entity Role

--- src/main/java/univpm/advprog/aule/model/entities/Role.java
+++ src/main/java/univpm/advprog/aule/model/entities/Role.java
@@ -2,6 +2,26 @@ package univpm.advprog.aule.model.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Role implements Serializable {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
 }
