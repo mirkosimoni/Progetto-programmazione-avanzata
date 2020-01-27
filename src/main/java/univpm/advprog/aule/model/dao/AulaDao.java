7f964dd65124e521161e14dc7c12409444699858
albertopierini <50133437+albertopierini@users.noreply.github.com>
2020-01-27 10:46:49 +0100

    AulaDao e AulaDaoDefault
    Implementazione delle classi

--- src/main/java/univpm/advprog/aule/model/dao/AulaDao.java
+++ src/main/java/univpm/advprog/aule/model/dao/AulaDao.java
@@ -1,6 +1,28 @@
 package univpm.advprog.aule.model.dao;
 
-// prova commit
-public interface AulaDao {
+import java.util.List;
+import java.util.Set;
 
+import org.hibernate.Session;
+
+import univpm.advprog.aule.model.entities.Aula;
+import univpm.advprog.aule.model.entities.Prenotation;
+
+public interface AulaDao {
+	
+	public Session getSession();
+	public void setSession(Session session);
+	
+	List<Aula> findAll();
+	
+	Aula findById(long id);
+	
+	Aula create(int numeroPosti, boolean presentiPrese);
+	
+	Aula update(Aula aula);
+	
+	void delete(Aula aula);
+	
+	Set<Prenotation> getPrenotazioni(Aula aula);
+	
 }
--- src/main/java/univpm/advprog/aule/model/dao/AulaDaoDefault.java
+++ src/main/java/univpm/advprog/aule/model/dao/AulaDaoDefault.java
@@ -1,5 +1,55 @@
 package univpm.advprog.aule.model.dao;
 
-public class AulaDaoDefault implements AulaDao {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Query;
+
+import univpm.advprog.aule.model.entities.Aula;
+import univpm.advprog.aule.model.entities.Prenotation;
+
+public class AulaDaoDefault extends DefaultDao implements AulaDao {
+
+	@Override
+	public List<Aula> findAll() {
+		return getSession().
+				createQuery("from Aula a", Aula.class).
+				getResultList();
+	}
+
+	@Override
+	public Aula findById(long id) {
+		return getSession().find(Aula.class, id);
+	}
+
+	@Override
+	public Aula create(int numeroPosti, boolean presentiPrese) {
+		Aula a = new Aula();
+		a.setNumeroPosti(numeroPosti);
+		a.setPresentiPrese(presentiPrese);
+		
+		this.getSession().save(a);
+		
+		return a;
+	}
+
+	@Override
+	public Aula update(Aula aula) {
+		Aula merged = (Aula)this.getSession().merge(aula);
+		return merged;
+	}
+
+	@Override
+	public void delete(Aula aula) {
+		this.getSession().delete(aula);
+		
+	}
+
+	@Override
+	public Set<Prenotation> getPrenotazioni(Aula aula) {
+		Query q = this.getSession().createQuery("From Prenotation p JOIN FETCH p.aula WHERE p.aula :aula", Prenotation.class);
+		return new HashSet<Prenotation>(q.setParameter("aula", aula).getResultList());
+	}
 
 }
