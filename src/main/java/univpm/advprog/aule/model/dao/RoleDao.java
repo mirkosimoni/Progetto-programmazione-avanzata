fc727b99017d6edce7812dac0a6f369b653f05fb
Fabio-Morganti <fabiomorga92@gmail.com>
2020-01-27 10:09:14 +0100

    Update RoleDao.java

--- src/main/java/univpm/advprog/aule/model/dao/RoleDao.java
+++ src/main/java/univpm/advprog/aule/model/dao/RoleDao.java
@@ -1,5 +1,18 @@
 package univpm.advprog.aule.model.dao;
 
-public interface RoleDao {
+import javax.management.relation.Role;
 
+import org.hibernate.Session;
+
+public interface RoleDao {
+	
+	Session getSession();
+	public void setSession(Session session);
+
+	Role create(String name);
+	
+	Role update(Role role);
+	
+	void delete(Role role);	
+	
 }
