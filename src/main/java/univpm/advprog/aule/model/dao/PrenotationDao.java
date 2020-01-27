0f607dcac8be2c154dcd512165895c8b23a61d32
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2020-01-27 10:19:42 +0100

    Update PrenotationDao

--- src/main/java/univpm/advprog/aule/model/dao/PrenotationDao.java
+++ src/main/java/univpm/advprog/aule/model/dao/PrenotationDao.java
@@ -1,5 +1,26 @@
 package univpm.advprog.aule.model.dao;
 
-public interface PrenotationDao {
+import java.util.List;
+import org.hibernate.Session;
 
+import org.joda.time.DateTime;
+
+import univpm.advprog.aule.model.entities.Prenotation;
+import univpm.advprog.aule.model.entities.User;
+import univpm.advprog.aule.model.entities.Aula;
+
+public interface PrenotationDao {
+	Session getSession();
+	public void setSession(Session session);
+	
+	List<Prenotation> findAll();
+	
+	Prenotation findById(Long id);
+	
+	Prenotation create(DateTime oraInizio, DateTime oraFine, User user, Aula aula, String nomeEvento, String note);
+	
+	Prenotation update(Prenotation prenotation);
+	
+	void delete(Prenotation prenotation);
+	
 }
