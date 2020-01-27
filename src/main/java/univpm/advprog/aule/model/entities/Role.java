cc3626b808c243c9c20a39b453ce456aba75cfd1
Fabio-Morganti <fabiomorga92@gmail.com>
2020-01-27 10:49:17 +0100

    Role
    Creato RoleDaoDefault
    Aggiornato Role

--- src/main/java/univpm/advprog/aule/model/entities/Role.java
+++ src/main/java/univpm/advprog/aule/model/entities/Role.java
@@ -3,9 +3,14 @@ package univpm.advprog.aule.model.entities;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "singer")
 
 public class Role implements Serializable {
 
