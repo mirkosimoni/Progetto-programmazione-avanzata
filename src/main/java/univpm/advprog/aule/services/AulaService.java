bc582b76fd4370389e95c351ff72ae6729cb75e4
albertopierini <50133437+albertopierini@users.noreply.github.com>
2020-02-12 10:04:53 +0100

    AuleService
    Implementazione interfaccia AuleService

--- src/main/java/univpm/advprog/aule/services/AulaService.java
+++ src/main/java/univpm/advprog/aule/services/AulaService.java
@@ -2,8 +2,39 @@ package univpm.advprog.aule.services;
 
 import java.util.List;
 
+import org.joda.time.DateTime;
+
 import univpm.advprog.aule.model.entities.Aula;
 
 public interface AulaService {
 
+	List<Aula> findAll();
+	
+	Aula findByNameQuota(String nome, int quota);
+	
+	Aula create(String nome, int quota, int numeroPosti, boolean presentiPrese);
+	
+	Aula update(Aula aula);
+	
+	Aula delete(Aula aula);
+	
+	Aula delete(long id);
+	
+	List<Aula> findLibere(DateTime oraInizio);
+	
+	List<Aula> findLibere(DateTime oraInizio, DateTime oraFine);
+	
+	List<Aula> findLiberePosti(DateTime oraInizio, int minimoPosti);
+	
+	List<Aula> findLiberePosti(DateTime oraInizio, DateTime oraFine, int minimoPosti);
+	
+	List<Aula> findLiberePrese(DateTime oraInizio, boolean presentiPrese);
+	
+	List<Aula> findLiberePrese(DateTime oraInizio, DateTime oraFine, boolean presentiPrese);
+	
+	List<Aula> findLibereQuota(DateTime oraInizio, int quota);
+	
+	List<Aula> findLibereQuota(DateTime oraInizio, DateTime oraFine, int quota);
+	
+	
 }
