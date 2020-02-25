bc95323d3e0bd983818829e69d911ac00dac8502
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2020-02-25 15:15:27 +0100

    Problema risolto adesso stampa la home

--- src/main/java/univpm/advprog/aule/controller/HomeController.java
+++ src/main/java/univpm/advprog/aule/controller/HomeController.java
@@ -15,25 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	@Autowired
-	String appName = "Aule app";
-
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		System.out.println("Home Page Requested, locale = " + locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-		
-		model.addAttribute("appName", appName);
-
+	public String home() {
 		return "home";
 	}
 
