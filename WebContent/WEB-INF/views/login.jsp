fe94288c001ac74fdd9709867a655238242796df
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2020-04-06 18:03:10 +0200

    Security

--- WebContent/WEB-INF/views/login.jsp
+++ WebContent/WEB-INF/views/login.jsp
@@ -7,22 +7,22 @@
 
 
 <div class="row">
-<div class="col-6 offset-3">
-<div class="jumbotron jumbotron-fluid">
-<div class="container">
-<h1 class="display-4">Login</h1>
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Username</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
-</div>
-</div>
+	<div class="col-6 offset-3">
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<h1 class="display-4">Login</h1>
+				<form name='login' action="<c:url value="/login" />" method='POST'>
+  					<div class="form-group">
+    					<label for="exampleInputEmail1">Username</label>
+    					<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
+  					</div>
+  					<div class="form-group">
+    					<label for="exampleInputPassword1">Password</label>
+    					<input type="password" class="form-control" id="exampleInputPassword1" name="password">
+  					</div>
+ 					<button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
---
+++ src/main/java/univpm/advprog/aule/security/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package univpm.advprog.aule.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import univpm.advprog.aule.services.UserServiceDefault;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().
+			antMatchers("/login").permitAll().
+			antMatchers("/").permitAll().
+			antMatchers("/instruments/**").hasAnyRole("ADMIN").
+			antMatchers("/**").hasAnyRole("USER").
+				and().formLogin().loginPage("/login").defaultSuccessUrl("/")
+					.failureUrl("/login?error=true").permitAll().
+				and().logout().logoutSuccessUrl("/") // NB se commentiamo
+														// questa riga,
+														// viene richiamata
+														// /login?logout
+					.invalidateHttpSession(true).permitAll().
+			and().csrf().disable();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserServiceDefault();
+	};
+
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+//		auth.inMemoryAuthentication().withUser("imuser")
+//			.password(this.passwordEncoder.encode("imuser"))
+//			.roles("USER");
+//		auth.inMemoryAuthentication().withUser("imadmin")
+//			.password(this.passwordEncoder.encode("imadmin"))
+//			.roles("USER", "ADMIN");
+
+		auth.userDetailsService(userDetailsService()).passwordEncoder(this.passwordEncoder);
+//		auth.userDetailsService(this.userDetailsService).passwordEncoder(this.passwordEncoder);
+	}
+
+}
