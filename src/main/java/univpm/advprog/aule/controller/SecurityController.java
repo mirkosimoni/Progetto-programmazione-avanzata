fc232127c3e698be645deef26797b9ab1bc451f2
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2020-04-06 17:44:36 +0200

    login view

---
+++ src/main/java/univpm/advprog/aule/controller/SecurityController.java
@@ -0,0 +1,15 @@
+package univpm.advprog.aule.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SecurityController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+}
