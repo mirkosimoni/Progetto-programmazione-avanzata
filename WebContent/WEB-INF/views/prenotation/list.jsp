10329937d0464de3d454594cb6c050587d081225
albertopierini <50133437+albertopierini@users.noreply.github.com>
2020-04-07 11:10:46 +0200

    List Prenotation Prova

---
+++ WebContent/WEB-INF/views/prenotation/list.jsp
@@ -0,0 +1,32 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Utente</th>
+      <th scope="col">Aula</th>
+      <th scope="col">Nome evento</th>
+      <th scope="col">Ora inizio</th>
+      <th scope="col">Ora fine</th>
+      <th scope="col">Note</th>
+    </tr>
+  </thead>
+  <tbody>
+  <c:forEach items="${prenotations}" var="p">
+    <tr>
+      <th scope="row">1</th>
+      <td>${p.user}</td>
+      <td>${p.aula}</td>
+      <td>${p.nomeEvento}</td>
+      <td>${p.oraInizio}</td>
+      <td>${p.oraFine}</td>
+      <td>${p.note}</td>
+    </tr>
+    </c:forEach>
+  </tbody>
+</table>
---
+++ src/main/java/univpm/advprog/aule/controller/PrenotationController.java
@@ -0,0 +1,49 @@
+package univpm.advprog.aule.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import univpm.advprog.aule.model.dao.*;
+import univpm.advprog.aule.model.entities.*;
+import univpm.advprog.aule.services.PrenotationService;
+
+@RequestMapping("/prenotations")
+@Controller
+public class PrenotationController {
+
+	private PrenotationService prenotationService;
+	
+	@GetMapping(value = "/list")
+	public String list(@RequestParam(value = "message", required=false) String message, Model uiModel) {
+
+		System.out.println("SIZE PRENOTATIONS");
+		List<Prenotation> allPrenotations = this.prenotationService.findAll();
+		
+		System.out.println(allPrenotations.size());
+		
+		uiModel.addAttribute("prenotations", allPrenotations);
+		//uiModel.addAttribute("numInstruments", allInstruments.size());
+		
+		// TODO ricevere un parametro via GET (es. per messaggio di esito operazione)
+		//uiModel.addAttribute("message", message);
+		
+		return "prenotations/list";
+	}
+	
+	
+	
+}
