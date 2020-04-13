1a110849930c9b5cf98ab0faf7307ac49650ae56
Lorenzo <melogica5@gmail.com>
2020-04-13 20:52:21 +0200

    Creazione classi e interfacce per Gestione Aule
    Stessa inizializzazione fatta per "Profilo"

---
+++ WebContent/WEB-INF/views/gestione aule/gestione-aule.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="sec"
+	uri="http://www.springframework.org/security/tags"%>
+<%@ page session="false"%>
+<%@ taglib prefix="sec"
+	uri="http://www.springframework.org/security/tags"%>
+	
+<body style= "background-color:rgb(52,58,64);">
+
+<div class="row">
+	<img src="<c:url value="/media/home.jpg"/>" class="img-fluid" alt="home" style="width: 100%;">
+</div>
+
+
+
+
+
+</body>
---
+++ src/main/java/univpm/advprog/aule/services/GestioneAuleServiceDefault.java
@@ -0,0 +1,33 @@
+package univpm.advprog.aule.services;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import univpm.advprog.aule.model.entities.Aula;
+import univpm.advprog.aule.model.entities.Prenotation;
+import univpm.advprog.aule.model.entities.Profile;
+import univpm.advprog.aule.model.entities.User;
+import univpm.advprog.aule.utils.PrenotationsOverlapFinder;
+import univpm.advprog.aule.model.dao.AulaDao;
+import univpm.advprog.aule.model.dao.PrenotationDao;
+import univpm.advprog.aule.model.dao.ProfileDao;
+import univpm.advprog.aule.model.dao.RoleDao;
+import univpm.advprog.aule.model.dao.UserDao;
+
+@Transactional
+@Service("gestioneauleService")
+public class GestioneAuleServiceDefault implements GestioneAuleService {
+	
+	ProfileDao profilerepository;
+	UserDao userRepository;
+	
+
+	}
+	
+
+
